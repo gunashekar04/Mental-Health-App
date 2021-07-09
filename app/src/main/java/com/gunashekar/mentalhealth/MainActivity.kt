2de98ca8c85c878cc77a9d131cc0f1765623d5df
gunashekar04 <gsr7845@gmail.com>
2021-07-09 20:09:03 +0530

    First commit

---
+++ app/src/main/java/com/gunashekar/mentalhealth/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gunashekar.mentalhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
